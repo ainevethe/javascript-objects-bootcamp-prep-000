f299bd64ccc5a020f1445a7d05550ccb2a62e107
Crista Cooper <aine3d@gmail.com>
2017-09-05 16:56:41 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,6 @@
 var playlist = { artistName: "artistName", songTitle: "songTitle" }
 
 function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
   return playlist
 }
