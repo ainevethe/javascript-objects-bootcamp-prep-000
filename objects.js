b091425cbd49d36d42821d841b4e4ef738a49733
Crista Cooper <aine3d@gmail.com>
2017-09-05 16:47:24 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = {artistName, SongTitle};
+var playlist = {artistName = "artistName", songTitle = "songTitle"};
 
 function updatePlaylist {
   Object.assign({}, playlist, {artistName: "artist"}, {songTitle: "song"})
