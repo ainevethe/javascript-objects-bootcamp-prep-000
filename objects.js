d084e9301fced6870da277db7b30a06c0fa82b5f
Crista Cooper <aine3d@gmail.com>
2017-09-05 16:58:45 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,6 +6,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 function removePlaylist(playlist, artistName, songTitle) {
-  delete artistName.songTitle
+  delete playlist[artistName] = songTitle
   return playlist
 }
